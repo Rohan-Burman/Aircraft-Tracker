fbe8a66d13ff4d3bdeb844d20a7692e19bbe7638
Rohan Burman <rohan.burman20@students.bhasvic.ac.uk>
2021-10-30 00:08:29 +0100

    Map completed

--- Script.js
+++ Script.js
@@ -1,1 +1,22 @@
-// JavaScript source code
+mapboxgl.accessToken = 'pk.eyJ1IjoicmIyMyIsImEiOiJja3R4MGtlMnExN2JrMnZtcmNnY2E4c2NxIn0.FeT3uCP4BX11Nm_zsPczzw';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/rb23/ckvcr41l82igb15o20zlmtz6i', // style URL
+    center: [50.0, 3.0], // starting position
+    zoom: 3 // starting zoom
+});
+
+// Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+    })
+);
+
+map.addControl(new mapboxgl.NavigationControl());
