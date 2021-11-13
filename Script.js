9a7a1c3576a1c5bb8a808e153487cdc7ef87b19e
Rohan-Burman <92298959+Rohan-Burman@users.noreply.github.com>
2021-11-13 13:34:01 +0000

    Trying Markers

--- Script.js
+++ Script.js
@@ -22,10 +22,10 @@ map.addControl(
 //Add navigation controls to map.
 map.addControl(new mapboxgl.NavigationControl());
 
-//Add location tracking of mouse pointer for testing purposes. Will be removed in final product
-// map.on('mousemove', (e) => {
-//     document.getElementById("coord").innerHTML = JSON.stringify(e.lngLat.wrap());
-// });
+// Add location tracking of mouse pointer for testing purposes. Will be removed in final product
+map.on('mousemove', (e) => {
+    document.getElementById("coord").innerHTML = JSON.stringify(e.lngLat.wrap());
+});
 
 // const layerList = document.getElementById("menu");
 // const inputs = layerList.getElementsByTagName("input");
@@ -38,86 +38,39 @@ map.addControl(new mapboxgl.NavigationControl());
 // }
 
 
-// Add a new Marker.
-const marker = new mapboxgl.Marker({
-    color: '#F84C4C' // color it red
-});
-
-// Define the animation.
-function animateMarker(timestamp) {
-    const radius = 20;
-
-    /* 
-    Update the data to a new position 
-    based on the animation timestamp. 
-    The divisor in the expression `timestamp / 1000` 
-    controls the animation speed.
-    */
-    marker.setLngLat([
-        Math.cos(timestamp / 1000) * radius,
-        Math.sin(timestamp / 1000) * radius
-    ]);
-
-    /* 
-    Ensure the marker is added to the map. 
-    This is safe to call if it's already added.
-    */
-    marker.addTo(map);
-
-    // Request the next frame of the animation.
-    requestAnimationFrame(animateMarker);
-}
-
-// Start the animation.
-requestAnimationFrame(animateMarker);
-
 map.on('load', () => {
-    // Add an image to use as a custom marker
+    // Load an image from an external URL.
     map.loadImage(
-        'planeMarker.png',
+        'https://docs.mapbox.com/mapbox-gl-js/assets/cat.png',
         (error, image) => {
             if (error) throw error;
-            map.addImage('custom-marker', image);
-            // Add a GeoJSON source with 2 points
-            map.addSource('points', {
+
+            // Add the image to the map style.
+            map.addImage('plane', image);
+
+            // Add a data source containing one point feature.
+            map.addSource('point', {
                 'type': 'geojson',
                 'data': {
                     'type': 'FeatureCollection',
                     'features': [{
-                            // feature for Mapbox DC
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [-77.03238901390978, 38.913188059745586]
-                            },
-                        },
-                        {
-                            // feature for Mapbox SF
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [-122.414, 37.776]
-                            },
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [0.0, 50.0]
                         }
-                    ]
+                    }]
                 }
             });
 
-            // Add a symbol layer
+            // Add a layer to use the image to represent the data.
             map.addLayer({
                 'id': 'points',
                 'type': 'symbol',
-                'source': 'points',
+                'source': 'point', // reference the data source
                 'layout': {
-                    'icon-image': 'custom-marker',
-                    // get the title name from the source's "title" property
-                    'text-field': ['get', 'title'],
-                    'text-font': [
-                        'Open Sans Semibold',
-                        'Arial Unicode MS Bold'
-                    ],
-                    'text-offset': [0, 1.25],
-                    'text-anchor': 'top'
+                    'icon-image': 'plane', // reference the image
+                    'icon-size': 0.25
                 }
             });
         }
