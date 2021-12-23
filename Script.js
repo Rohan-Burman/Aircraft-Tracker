9f3b87cf42f1733bb4a3fffd46ea5db158f25d5c
Rohan-Burman <92298959+Rohan-Burman@users.noreply.github.com>
2021-12-23 01:45:02 +0000

    Map and graphics work

--- Script.js
+++ Script.js
@@ -1,12 +1,19 @@
+//Creating the map and adding controls.
+
 mapboxgl.accessToken = 'pk.eyJ1IjoicmIyMyIsImEiOiJja3R4MGtlMnExN2JrMnZtcmNnY2E4c2NxIn0.FeT3uCP4BX11Nm_zsPczzw';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/rb23/ckvcr41l82igb15o20zlmtz6i', // style URL
     center: [0.0, 50.0], // starting position
     zoom: 3, // starting zoom
+    minZoom: 1.5, // minimum zoom
 });
 
+//Disabling rotation of the map and enabling keyboard navigation controls.
 map.dragRotate.disable();
+map.keyboard.enable();
+map.keyboard.disableRotation();
+
 // Add geolocate control to the map.
 map.addControl(
     new mapboxgl.GeolocateControl({
@@ -20,7 +27,6 @@ map.addControl(
     })
 );
 
-
 //Add navigation controls to map.
 map.addControl(new mapboxgl.NavigationControl());
 
@@ -29,6 +35,7 @@ map.on('mousemove', (e) => {
     document.getElementById("coord").innerHTML = JSON.stringify(e.lngLat.wrap());
 })
 
+//Initalising global variables. Each marker graphic will have its own relevant information.
 var icao = "";
 var callsign = "";
 var longitude = 0;
@@ -37,33 +44,22 @@ var velocity = "";
 var track = 0;
 var altitude = "";
 var coordinates = [];
-var markerNumber = 0;
 var currentMarkers = [];
 
 
-/* var geojson = {
-    'type': 'Feature',
-    'icao': icao,
-    'callsign': callsign,
-    'geometry': {
-        'type': 'Point',
-        'coordinates': [longitude, latitude],
-        'track': track,
-        'velocity': velocity,
-        'altitude': altitude,
-    }
-} */
-
-const urlImage = "planeMarker.png";
-
-function removeGraphic() {
+//
+function clearMarkers() {
     if (currentMarkers !== null) {
         currentMarkers.forEach((marker) => marker.remove());
         currentMarkers = [];
+        delete geojson;
     }
 }
 
-async function getFlights() {
+//Gets data of aircraft from API, assigns it to variables and makes marker graphic.
+
+
+async function getData() {
     const url = "https://opensky-network.org/api/states/all";
 
     key = 0;
@@ -71,8 +67,8 @@ async function getFlights() {
     var response = await fetch(url);
     var data = await response.json();
 
-    //Loops through the elements in the json response to assign needed information to geojson for each iteration.
-    //console.log(data);
+
+    //Loops throuugn the API response and assigns variables for each flight to be made into a marker graphic.
     for (var key in data.states) {
         //Assigning data from the API into varibles.
         icao = data.states[key][0];
@@ -80,14 +76,16 @@ async function getFlights() {
         longitude = data.states[key][5];
         latitude = data.states[key][6];
         velocity = data.states[key][9];
-        track = data.states[key][11];
+        track = data.states[key][10];
         altitude = data.states[key][13];
 
         coordinates = [longitude, latitude];
 
+        //Creating a HTML class for each marker.
         var marker = document.createElement("div");
         marker.className = "marker";
 
+        //Creates the marker in correct position and rotation, adding it to the currentMarkers array.
         var graphic = new mapboxgl.Marker(marker)
             .setLngLat(coordinates)
             .setRotation(track)
@@ -97,7 +95,13 @@ async function getFlights() {
     }
 }
 
-/* map.on("load", () => { getFlights() }); */
-getFlights();
-setInterval(getFlights, 5000); //Calls the getFlights function every 10s. Will be lowered, but currently used for testing purposes.
-setInterval(removeGraphic, 7500);
+/* map.on("load", () => { getData() }); */
+getData();
+setInterval(getData, 5000); //Calls the getData function every 10s. Will be lowered, but currently used for testing purposes.
+setInterval(clearMarkers, 7500);
+
+
+map.on("load", () => { getData() });
+getData();
+setInterval(getData, 5000); //Calls the getData function every 10s. Will be lowered, but currently used for testing purposes.
+setInterval(clearMarkers, 7500);
